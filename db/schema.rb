be141b9fcdbc21ed62a9a26426923f035df2cfba
Joe Clark <weirdlookingjay@live.com>
2023-04-08 13:36:32 -0400

    Added internet speed table and related associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_172938) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_173304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "internet_speeds", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "place_id", null: false
+    t.decimal "download_speed", precision: 15, scale: 2, null: false
+    t.string "download_units", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_internet_speeds_on_place_id"
+  end
 
   create_table "places", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
